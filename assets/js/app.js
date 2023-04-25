b94593938f342cfc6bf8d96f41008e7f58ac4d8d
Einar Engstrom <meengst@gmail.com>
2023-04-24 21:25:57 -0400

    ready for regex

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,8 @@ import "../css/app.css"
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 import * as Tone from "../vendor/tone.js"
 
@@ -37,108 +37,121 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.main.js';
 
 
 let Hooks = {
-    BasicPlay: {
-      mounted() {
-        this.handleEvent("update_score", (params) => {
-          const synth = new Tone.PolySynth();
-          const synth2 = new Tone.FMSynth();
-          const vibrato = new Tone.Vibrato(params.vibratoFrequency, params.vibratoDepth);
-          const chebyshev = new Tone.Chebyshev(params.chebyshev); // range 1-100
-          const crusher = new Tone.BitCrusher(params.crusher); //range 1-16
-          const filter = new Tone.Filter(params.filterFrequency, params.filterType, params.filterRolloff); //rolloff -12/-24/-48/-96 types "lowpass", "highpass", "bandpass", "lowshelf", "highshelf", "notch", "allpass", or "peaking"
-          const panner = new Tone.Panner(params.panner); // -1 to 1
-          const delay = new Tone.PingPongDelay(params.delayTime, params.delayFeedback); //time and delay both 0 to 1
-          const reverb = new Tone.Reverb(params.reverbDecay, params.reverbWet);
-          const compressor = new Tone.Compressor(-30, 4);
-            
-          /**
-           * Audio effects chain:
-          *
-          * [PolySynth] --> [Vibrato] --> [Chebyshev] --> [Crusher] --> [Filter] --> ... --> Output
-            */
-          synth.connect(vibrato);
-          vibrato.connect(chebyshev);
-          chebyshev.connect(crusher);
-          crusher.connect(compressor);
-          // filter.connect(panner);
-          // panner.connect(delay);
-          // delay.connect(reverb);
-          // reverb.connect(compressor);
-          compressor.toDestination();
-          
-          synth2.connect(delay);
-          delay.connect(reverb);
-          reverb.connect(compressor);
-          compressor.toDestination();
-  
-          // synth.triggerAttackRelease(params.note1, "8t");
-          
-          const seq = new Tone.Sequence((time, note) => {
-            synth.triggerAttackRelease(note, 0.1, time);
-            // subdivisions are given as subarrays
-          }, ["C2", params.note1, params.note2, ["Ab4", "Fb4"], "Gb4", params.note2]).start(0);
-          Tone.Transport.start();
-          
-          // const now = Tone.now()
-          // synth.triggerAttackRelease(params.note1, "8t", now);
-          // synth2.triggerAttackRelease(params.note2, "4n", now + 0.3);
-          // synth.triggerAttackRelease(params.note1, "2n", now + 0.5);
-          // synth2.triggerAttackRelease(params.note2, "8t", now + 1.1);
-          
-          //LOOP
-          // const loopA = new Tone.Loop(time => {
-            //   synth2.triggerAttackRelease(params.note2, "8t", time);
-            // }, "1n").start(0);
-          // Tone.Transport.start();
-          // Tone.Transport.bpm.rampTo(800, 20);
-  
-          console.log(params)
-        })
-  
-        // this.el.addEventListener("input", () => {
-        // }) 
-      }
-    },
-    // ClickHook: {
-    //   mounted() {
-    //     this.el.addEventListener("click", () => {
-    //       console.log(editor.getValue())
-    //     })
-    //   }
-    // },
-    Editor: {
-      mounted() {
+  BasicPlay: {
+    mounted() {
+      this.handleEvent("update_score", (params) => {
+        const synth = new Tone.PolySynth();
+        const synth2 = new Tone.FMSynth();
+        const vibrato = new Tone.Vibrato(params.vibratoFrequency, params.vibratoDepth);
+        const chebyshev = new Tone.Chebyshev(params.chebyshev); // range 1-100
+        const crusher = new Tone.BitCrusher(params.crusher); //range 1-16
+        const filter = new Tone.Filter(params.filterFrequency, params.filterType, params.filterRolloff); //rolloff -12/-24/-48/-96 types "lowpass", "highpass", "bandpass", "lowshelf", "highshelf", "notch", "allpass", or "peaking"
+        const panner = new Tone.Panner(params.panner); // -1 to 1
+        const delay = new Tone.PingPongDelay(params.delayTime, params.delayFeedback); //time and delay both 0 to 1
+        const reverb = new Tone.Reverb(params.reverbDecay, params.reverbWet);
+        const compressor = new Tone.Compressor(-30, 4);
+
+        /**
+         * Audio effects chain:
+        *
+        * [PolySynth] --> [Vibrato] --> [Chebyshev] --> [Crusher] --> [Filter] --> ... --> Output
+          */
+        synth.connect(vibrato);
+        vibrato.connect(chebyshev);
+        chebyshev.connect(crusher);
+        crusher.connect(compressor);
+        // filter.connect(panner);
+        // panner.connect(delay);
+        // delay.connect(reverb);
+        // reverb.connect(compressor);
+        compressor.toDestination();
+
+        synth2.connect(delay);
+        delay.connect(reverb);
+        reverb.connect(compressor);
+        compressor.toDestination();
+
+        // synth.triggerAttackRelease(params.note1, "8t");
+
+        const seq = new Tone.Sequence((time, note) => {
+          synth.triggerAttackRelease(note, 0.1, time);
+          // subdivisions are given as subarrays
+        }, ["C2", params.note1, params.note2, ["Ab4", "Fb4"], "Gb4", params.note2]).start(0);
+        Tone.Transport.start();
+
+        // const now = Tone.now()
+        // synth.triggerAttackRelease(params.note1, "8t", now);
+        // synth2.triggerAttackRelease(params.note2, "4n", now + 0.3);
+        // synth.triggerAttackRelease(params.note1, "2n", now + 0.5);
+        // synth2.triggerAttackRelease(params.note2, "8t", now + 1.1);
+
+        //LOOP
+        // const loopA = new Tone.Loop(time => {
+        //   synth2.triggerAttackRelease(params.note2, "8t", time);
+        // }, "1n").start(0);
+        // Tone.Transport.start();
+        // Tone.Transport.bpm.rampTo(800, 20);
+
+        console.log(params)
+      })
+
+      // this.el.addEventListener("input", () => {
+      // }) 
+    }
+  },
+
+  Editor: {
+    mounted() {
         let editor = monaco.editor.create(this.el, {
-          value: ['# PipeSines', '# software for writing music in pure Elixir', '# start coding/composing here'].join('\n'),
-          language: 'elixir',
-          theme: "vs-dark"
-        });
-        
-        editor.onDidChangeModelContent(() => {
-          //* add push event here
-          console.log(editor.getValue())
-        })
-      }
+        value: ['# PipeSines', '# software for writing music in pure Elixir', '# start coding/composing here', '# ctrl + v to evaluate'].join('\n'),
+        language: 'elixir',
+        theme: "vs-light"
+      });
+
+      editor.onDidPaste(() => {
+        console.log("BEING TRIGGERED")
+        this.pushEvent("perform", {score: editor.getValue()})
+      });
+      
+      this.handleEvent("update_score", (params) => {
+        const synth = new Tone.Synth().toDestination();
+        synth.triggerAttackRelease("C4", "8n");
+        console.log("HE:LLO SUGCCCESSSS")
+      })
     }
   }
-  
-  
-  let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-  let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, hooks: Hooks })
-  
-  // Show progress bar on live navigation and form submits
-  topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
-  window.addEventListener("phx:page-loading-start", info => topbar.show())
-  window.addEventListener("phx:page-loading-stop", info => topbar.hide())
-  // window.addEventListener("phx:perform", event => {
-  //     let params = event.detail
-  // })
-  
-  // connect if there are any LiveViews on the page
-  liveSocket.connect()
-  
-  // expose liveSocket on window for web console debug logs and latency simulation:
-  // >> liveSocket.enableDebug()
-  // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-  // >> liveSocket.disableLatencySim()
-  window.liveSocket = liveSocket
+}
+
+
+// editor.addEventListener("keydown", e => {
+//   e.preventDefault();
+
+//   if (
+//     e.key.toLowerCase() === "p"
+//     && e.ctrlKey
+//     && e.altKey
+//   ) {
+//   this.pushEvent("send-text", editor.getValue())
+//   }
+// });
+
+
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, hooks: Hooks })
+
+// Show progress bar on live navigation and form submits
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
+window.addEventListener("phx:page-loading-start", info => topbar.show())
+window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+// window.addEventListener("phx:perform", event => {
+//     let params = event.detail
+// })
+
+// connect if there are any LiveViews on the page
+liveSocket.connect()
+
+// expose liveSocket on window for web console debug logs and latency simulation:
+// >> liveSocket.enableDebug()
+// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
+// >> liveSocket.disableLatencySim()
+window.liveSocket = liveSocket
--- lib/pipesine_web/live/pipesine_live.ex
+++ lib/pipesine_web/live/pipesine_live.ex
@@ -8,50 +8,59 @@ defmodule PipesineWeb.PipesineLive do
   @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~H"""
-    <div id="container" class="background" style="width: 1200px; height: 600px; border: 8px solid black" phx-hook="Editor"></div>
-      <button phx-click="perform">PERFORM</button>
+      <div id="container" class="background" style="width: 1200px; height: 600px; border: 9px solid black" phx-hook="Editor"></div>
     """
   end
 
+    # <div style="">
+    #   <.form let={f} for={:perform_form} phx-change="perform">
+    #     <%= text_input f, :perform, id: "basic-play", phx_hook: "BasicPlay" %>
+    #   </.form>
+    # </div>
+
+  # <button phx-click="perform">PERFORM</button>
+
   # <%= submit "PERFORM", :perform, id: "basic-play", phx_hook: "BasicPlay" %>
   # <button id="click-hook" phx-hook="ClickHook">PERFORM</button>
-  # <div style="">
-  #   <.form let={f} for={:perform_form} phx-submit="perform">
-  #     <%= text_input f, :perform, id: "basic-play", phx_hook: "BasicPlay" %>
-  #   </.form>
-  # </div>
 
   # def handle_event("ping", _params, socket) do
   #   {:noreply, push_event(socket, "pong", %{})}
   # end
 
-  def handle_event("perform", _params, socket) do
-    IO.inspect("I DID NOT BREAK", label: "Button test")
-    {:noreply, socket}
-  end
-
-  # def handle_event("perform", params, socket) do
-  #   metascore = params["perform_form"]
-  #   score = metascore["perform"]
-
-  #   scanned_score = Regex.scan(~r/\w/, score)
-  #   flat_scan = List.flatten(scanned_score)
-
-  #   bits = abs(Enum.count(flat_scan) - 16)
-  #   crusher =
-  #     cond do
-  #       bits <= 16 && bits >=4 -> bits
-  #       true -> 16
-  #     end
-
-  #   chebs = List.last(flat_scan)
-  #   # note2 = "F3"
-
-  #   IO.inspect(flat_scan)
-  #   IO.inspect(chebs)
-
-  #   {:noreply, push_event(socket, "update_score", %{note1: ["Eb3", "Gb3", "C4"], note2: "F3", crusher: 16, delayTime: 0.4, delayFeedback: 0.6})}
-  #   # {:noreply, assign(socket, :score, params)}
+  # def handle_event("perform", _params, socket) do
+  #   IO.inspect("I DID NOT BREAK", label: "Button test")
+  #   {:noreply, socket}
   # end
+
+  # def handle_event("send-text", params, socket) do
+  #   IO.inspect("I GOT THE TEXT", label: "Button test")
+  #   IO.inspect(params)
+  #   {:noreply, push_event(socket, "update_score", %{note1: ["Eb3", "Gb3", "C4"], note2: "F3", crusher: 16, delayTime: 0.4, delayFeedback: 0.6})}
+  # end
+
+  def handle_event("perform", params, socket) do
+    IO.inspect(params)
+    # metascore = params["perform_form"]
+    # score = metascore["perform"]
+
+    # scanned_score = Regex.scan(~r/\w/, score)
+    # flat_scan = List.flatten(scanned_score)
+
+    # bits = abs(Enum.count(flat_scan) - 16)
+    # crusher =
+    #   cond do
+    #     bits <= 16 && bits >=4 -> bits
+    #     true -> 16
+    #   end
+
+    # chebs = List.last(flat_scan)
+    # # note2 = "F3"
+
+    # IO.inspect(flat_scan)
+    # IO.inspect(chebs)
+
+    {:noreply, push_event(socket, "update_score", %{note1: ["Eb3", "Gb3", "C4"], note2: "F3", crusher: 16, delayTime: 0.4, delayFeedback: 0.6})}
+    # {:noreply, assign(socket, :score, params)}
+  end
 
 end
