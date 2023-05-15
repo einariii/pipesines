c2691586ff170fce0618d0ac5e69f7c3e008f59f
Einar Engstrom <meengst@gmail.com>
2023-05-14 22:27:11 -0400

    new sine waves and modal style

--- lib/pipesine_web/live/about_component.ex
+++ lib/pipesine_web/live/about_component.ex
@@ -17,11 +17,11 @@ defmodule PipesineWeb.PipesineLive.AboutComponent do
       <h1>Pipesines. Elixir as musical instrument.</h1>
       Pipesines (v0.1.0) is a web app that encourages Elixir developers to explore sound through code: general programming as a specific sonic experience.
 
-      What do |> sound like? Which is more musical: if, case, or cond? Will Enum.filter actually filter a sound? And what about Enum.reduce?
+      What do <code class="coded">|></code> sound like? Which is more musical: <code class="coded">if</code>, <code class="coded">case</code>, or <code class="coded">cond</code>? Will <code class="coded">Enum.filter</code> actually filter a sound? And what about <code class="coded">Enum.reduce</code>?
 
       <code class="coded">assert compose(Elixir) == compose(music)</code>
 
-      Paste existing code into the text editor, or write something from scratch. Type Alt+P to hear how it sounds. Initially it may seem like a chaotic mess. Type Alt+P again to pause. Adding, deleting, or refactoring may lead to more pleasant, harmonious, or musical output. Or not! What sounds "good" is a subjective judgment. What do your coder ears prefer?
+      Paste existing code into the text editor, or write something from scratch. Type <code class="coded">alt+p</code> to hear how it sounds. Initially it may seem like a chaotic mess. Type <code class="coded">alt+p</code> again to pause. Adding, deleting, or refactoring may lead to more pleasant, harmonious, or musical output. Or not! What sounds "good" is a subjective judgment. What do your coder ears prefer?
 
       Create an account to add your code/compositions to the community database. Save an entire <code class="coded">defmodule</code>, or just one line!
 
