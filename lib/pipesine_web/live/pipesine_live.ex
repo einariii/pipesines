7d46386ddd8e2c6e9bc13b16e989e4c51a2c0dbe
Einar Engstrom <meengst@gmail.com>
2023-10-31 01:48:58 -0400

    minimal step 1

--- lib/pipesine_web/live/pipesine_live.ex
+++ lib/pipesine_web/live/pipesine_live.ex
@@ -2,12 +2,12 @@ defmodule PipesineWeb.PipesineLive do
   use PipesineWeb, :live_view
   import Pipesine.Composers
 
-  def mount(params, session, socket) do
-    composer_id =
-      if session["composer_token"],
-        do:
-          get_composer_by_session_token(session["composer_token"]).id
-          |> IO.inspect(label: "COMPOSERID")
+  def mount(params, _session, socket) do
+    # composer_id =
+    #   if session["composer_token"],
+    #     do:
+    #       get_composer_by_session_token(session["composer_token"]).id
+    # |> IO.inspect(label: "COMPOSERID")
 
     # composer_username =
     #   if session["composer_token"],
@@ -28,7 +28,7 @@ defmodule PipesineWeb.PipesineLive do
       socket
       |> assign(display_modal: false)
       |> assign(score: score)
-      |> assign(composer_id: composer_id)
+      # |> assign(composer_id: composer_id)
       # |> assign(composer_username: composer_username)
       # |> assign(composer_email: composer_email)
     }
@@ -37,32 +37,38 @@ defmodule PipesineWeb.PipesineLive do
   @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~H"""
-    <div>
-    <div class="tooltip versions"><button class="krub">REGEX mode</button>
-    <span class="tooltiptext">current default<br>PCRE regular expressions!</span>
-    </div>
-    <div class="tooltip versions"><button class="krub">AST mode</button>
-    <span class="tooltiptext">future implementation<br>ABSTRACT SYNTAX TREE!</span>
-    </div>
-    <div class="tooltip versions"><button class="krub">ML mode</button>
-    <span class="tooltiptext">future implementation<br>MACHINE LEARNING!</span>
-    </div>
-    <div id="container" class="filtered" style="width: 1200px; height: 650px; border: 9px solid black" phx-hook="Editor"></div>
-    <div class="tooltip versions"><button class="krub" phx-click="perform" style="margin-top: 8px">play code</button>
-      <span class="tooltiptext">TO HEAR YOUR ELIXIR<br>type alt+p (opt+p on mac)<br>in the editor!</span>
-    </div>
-    <div class="tooltip versions"><button class="krub" phx-click="save" style="margin-top: 8px">save composition</button>
-      <span class="tooltiptext"><i>registered users click here to persist your code in the community database!</i></span>
-    </div>
-    <div class="tooltip versions"><button class="krub" phx-click="perform" style="margin-top: 8px">it sounds glitchy</button>
-      <span class="tooltiptext">LET IT GLITCH<br>
-      <br>
-      ...or refresh the page</span>
-    </div>
-    </div>
-    <div>
+    <div class="text-editor">
+      <div class="fake-buttons">
+        <div class="tooltip versions"><button class="krub" disabled>REGEX mode</button>
+          <span class="tooltiptext">current default<br>PCRE regular expressions!</span>
+        </div>
+        <div class="tooltip versions"><button class="krub" disabled>AST mode</button>
+          <span class="tooltiptext">future implementation<br>ABSTRACT SYNTAX TREE!</span>
+        </div>
+        <div class="tooltip versions"><button class="krub" disabled>ML mode</button>
+          <span class="tooltiptext">future implementation<br>MACHINE LEARNING!</span>
+        </div>
+      </div>
 
+      <div id="container" class="filtered" style="width: 1200px; height: 650px; border: 9px solid black" phx-hook="Editor"></div>
+
+      <div class="fake-buttons">
+        <div class="tooltip versions"><button class="krub" style="margin-top: 8px" disabled>play code</button>
+          <span class="tooltiptext">TO HEAR YOUR ELIXIR<br>type alt+p (opt+p on mac)<br>in the editor!</span>
+        </div>
+        <div class="tooltip versions"><button class="krub" style="margin-top: 8px" disabled>it sounds glitchy</button>
+          <span class="tooltiptext">LET IT GLITCH<br>
+          <br>
+          ...or refresh the page</span>
+        </div>
+        <div class="tooltip versions"><button class="krub" style="margin-top: 8px" disabled>scales</button>
+          <span class="tooltiptext">set scale on the first line.
+          options = 22_edo, bohlen_pierce, sa_murcchana, tonality_diamond, just_intonation, pentatonic.
+          default = superpyth</span>
+        </div>
+      </div>
     </div>
+
     <%= if @live_action == :about do %>
       <.modal>
         <.live_component module={PipesineWeb.PipesineLive.AboutComponent} id={@display_modal} />
