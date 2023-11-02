e4bf39825bcb34200dd3a46d8a9ca77bb5574d69
Einar Engstrom <meengst@gmail.com>
2023-11-02 01:58:30 -0400

    redesign plus start new modal rendering

--- lib/pipesine_web/live/pipesine_live.ex
+++ lib/pipesine_web/live/pipesine_live.ex
@@ -26,7 +26,7 @@ defmodule PipesineWeb.PipesineLive do
     {
       :ok,
       socket
-      |> assign(display_modal: false)
+      |> assign(toggle_modal: nil)
       |> assign(score: score)
       # |> assign(composer_id: composer_id)
       # |> assign(composer_username: composer_username)
@@ -36,32 +36,49 @@ defmodule PipesineWeb.PipesineLive do
 
   @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
+    IO.inspect(assigns, label: "ASSINGS IN LV")
+
     ~H"""
+    <div class="vt323">
+      <nav class="menuright">
+        <button phx-click="toggle_about">:about</button>
+        <button phx-click="toggle_ethos">:ethos</button>
+        <button phx-click="toggle_tekne">:tekne</button>
+        <button phx-click="toggle_label">:label</button>
+
+        <%# <PipesineWeb.Components.link href="/about">:about</PipesineWeb.Components.link> %>
+        <%# <PipesineWeb.Components.link href="/ethos">:ethos</PipesineWeb.Components.link> %>
+        <%# <PipesineWeb.Components.link href="/technique">:technique</PipesineWeb.Components.link> %>
+        <%# <PipesineWeb.Components.link href="/label">:label</PipesineWeb.Components.link> %>
+        <%# <PipesineWeb.Components.link href="/compositions">:compositions</PipesineWeb.Components.link>
+        <a class=""><%= render "_composer_menu.html", assigns </a> %>
+      </nav>
+    </div>
     <div class="text-editor">
       <div class="fake-buttons">
-        <div class="tooltip versions"><button class="krub" disabled>REGEX mode</button>
+        <div class="tooltip versions"><button class="vt323" disabled>REGEX mode</button>
           <span class="tooltiptext">current default<br>PCRE regular expressions!</span>
         </div>
-        <div class="tooltip versions"><button class="krub" disabled>AST mode</button>
+        <div class="tooltip versions"><button class="vt323" disabled>AST mode</button>
           <span class="tooltiptext">future implementation<br>ABSTRACT SYNTAX TREE!</span>
         </div>
-        <div class="tooltip versions"><button class="krub" disabled>ML mode</button>
+        <div class="tooltip versions"><button class="vt323" disabled>ML mode</button>
           <span class="tooltiptext">future implementation<br>MACHINE LEARNING!</span>
         </div>
       </div>
 
-      <div id="container" class="filtered" style="width: 1400px; height: 650px; border: 3px solid #957FEF" phx-hook="Editor"></div>
+      <div id="container" class="filtered" style="width: 120rem; height: 60rem; border: 3px solid #000" phx-hook="Editor"></div>
 
       <div class="fake-buttons">
-        <div class="tooltip versions"><button class="krub" style="margin-top: 8px" disabled>play code</button>
+        <div class="tooltip versions"><button class="vt323" style="margin-top: 8px" disabled>play code</button>
           <span class="tooltiptext">TO HEAR YOUR ELIXIR<br>type alt+p (opt+p on mac)<br>in the editor!</span>
         </div>
-        <div class="tooltip versions"><button class="krub" style="margin-top: 8px" disabled>it sounds glitchy</button>
+        <div class="tooltip versions"><button class="vt323" style="margin-top: 8px" disabled>it sounds glitchy</button>
           <span class="tooltiptext">LET IT GLITCH<br>
           <br>
           ...or refresh the page</span>
         </div>
-        <div class="tooltip versions"><button class="krub" style="margin-top: 8px" disabled>scales</button>
+        <div class="tooltip versions"><button class="vt323" style="margin-top: 8px" disabled>scales</button>
           <span class="tooltiptext">set scale on the first line.
           options = 22_edo, bohlen_pierce, sa_murcchana, tonality_diamond, just_intonation, pentatonic.
           default = superpyth</span>
@@ -69,24 +86,28 @@ defmodule PipesineWeb.PipesineLive do
       </div>
     </div>
 
-    <%= if @live_action == :about do %>
-      <.modal>
-        <.live_component module={PipesineWeb.PipesineLive.AboutComponent} id={@display_modal} />
+    <%= if @toggle_modal == :about do %>
+      <.modal return_to={Routes.pipesine_path(@socket, :index)}>
+        <.live_component
+          module={PipesineWeb.PipesineLive.AboutComponent}
+          id="about_modal"
+          return_to={Routes.pipesine_path(@socket, :index)}
+        />
       </.modal>
     <% end %>
-    <%= if @live_action == :ethos do %>
+    <%= if @toggle_modal == :ethos do %>
       <.modal>
-        <.live_component module={PipesineWeb.PipesineLive.EthosComponent} id={@display_modal} />
+        <.live_component module={PipesineWeb.PipesineLive.EthosComponent} id="ethos_modal" />
       </.modal>
     <% end %>
-    <%= if @live_action == :label do %>
+    <%= if @toggle_modal == :label do %>
       <.modal>
-        <.live_component module={PipesineWeb.PipesineLive.LabelComponent} id={@display_modal} />
+        <.live_component module={PipesineWeb.PipesineLive.LabelComponent} id="tekne_modal" />
       </.modal>
     <% end %>
-    <%= if @live_action == :technique do %>
+    <%= if @toggle_modal== :technique do %>
       <.modal>
-        <.live_component module={PipesineWeb.PipesineLive.TechniqueComponent} id={@display_modal} />
+        <.live_component module={PipesineWeb.PipesineLive.TechniqueComponent} id="label_modal" />
       </.modal>
     <% end %>
     """
@@ -118,5 +139,37 @@ defmodule PipesineWeb.PipesineLive do
     end
 
     {:noreply, socket}
+  end
+
+  def handle_event("toggle_about", _value, socket) do
+    {:noreply, assign(socket, toggle_modal: :about)}
+  end
+
+  def handle_event("toggle_ethos", _value, socket) do
+    {:noreply, assign(socket, toggle_modal: :ethos)}
+  end
+
+  def handle_event("toggle_tekne", _value, socket) do
+    {:noreply, assign(socket, toggle_modal: :tekne)}
+  end
+
+  def handle_event("toggle_label", _value, socket) do
+    {:noreply, assign(socket, toggle_modal: :label)}
+  end
+
+  def handle_event("close_about", _value, socket) do
+    {:noreply, assign(socket, toggle_modal: nil)}
+  end
+
+  def handle_event("close_ethos", _value, socket) do
+    {:noreply, assign(socket, toggle_modal: nil)}
+  end
+
+  def handle_event("close_tekne", _value, socket) do
+    {:noreply, assign(socket, toggle_modal: nil)}
+  end
+
+  def handle_event("close_label", _value, socket) do
+    {:noreply, assign(socket, toggle_modal: nil)}
   end
 end
